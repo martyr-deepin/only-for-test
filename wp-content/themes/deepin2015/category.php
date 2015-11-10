a126b0e6a798a73cfcb445945b0e4899ec7766e1
qiujieqiong <qiujieqiong@linuxdeepin.com>
2015-11-10 16:02:40 +0800

    test
    Change-Id: Ia4458b6abba6d3909d74df04e7089a599acbaba2

--- wp-content/themes/deepin2015/category.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-<link href="<?php bloginfo('template_url'); ?>/css/hot_sort.css" rel="stylesheet">
-<link href="<?php bloginfo('template_url'); ?>/css/add_sort.css" rel="stylesheet">
-<?php get_template_part('nav', 'clearfix'); ?>
-<?php get_template_part('loop', 'index' ); ?>
-<?php get_footer(); ?>
